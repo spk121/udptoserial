d27a8fc6e6bee2d7ee578cefb49ac2e8f55e4dde
Mike Gran <spk121@yahoo.com>
2017-11-28 17:14:56 -0800

    test out boost log

---
+++ hack/build.bat
@@ -0,0 +1,1 @@
+ g++ logtest.cpp -std=gnu++11 -lboost_log -lpthread -lboost_thread
---
+++ hack/logtest.cpp
@@ -0,0 +1,41 @@
+#define BOOST_LOG_DYN_LINK
+
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+
+namespace logging = boost::log;
+namespace src = boost::log::sources;
+namespace sinks = boost::log::sinks;
+namespace keywords = boost::log::keywords;
+
+
+
+void init()
+{
+  logging::add_file_log
+    (
+     keywords::file_name = "sample.log"
+     );
+
+  logging::core::get()->set_filter
+    (
+     logging::trivial::severity >= logging::trivial::info
+     );
+}
+
+int main (int, char*[])
+{
+  init();
+  logging::add_common_attributes();
+
+  using namespace logging::trivial;
+  src::severity_logger<severity_level> lg;
+
+  BOOST_LOG_SEV(lg, warning) << "A WARNING!!!!";
+}
+
